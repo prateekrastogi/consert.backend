7ee76d3598adef26f01714cda96a2eb35e018370
Prateek Rastogi <p.rastogi@outlook.com>
2017-05-07 04:20:29 +0530

    Updated access keys.

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -10,7 +10,7 @@ module.exports = function () {
   app.configure(authentication({
     idField: '_id',
     token: {
-      secret: 'cbvAOK+wGVhei6R9kDiumubYJYX+s8gJI2ZKOSyjH37zsTx7KVH6zO3Fgn0kfaIGw6SXoaVVbwiLxvbsEq1C2A=='
+      secret: 'cbvAOK+wGVhei6R9kDiumubYJYX+s8gJI2ZKOS|making|plans|yjH37zsTx7KVH6zO3Fgn0kfaIGw6SXoaVVbwiLxvbsEq1C2A=='
     },
     local: false,
     shouldSetupSuccessRoute: false,
@@ -18,7 +18,7 @@ module.exports = function () {
     spotify: {
       strategy: SpotifyStrategy,
       'clientID': '5084d28ce6924d7b98884323abc76109',
-      'clientSecret': '97e2cf30dd2d4f2b9f9b3cf40bbbfcc0',
+      'clientSecret': '0ba774af606549a58d1d3737c11d5d2a',
       'permissions': {
         authType: 'rerequest',
         'scope': ['user-top-read', 'user-read-birthdate', 'user-library-read', 'user-library-modify', 'user-follow-read', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public', 'playlist-modify-private', 'user-follow-modify', 'user-read-email', 'user-read-private']
