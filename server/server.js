68327ff3097fce81de900b3661812f278119b153
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-03 09:44:25 +0530

    Changed the code style to standard and refactored code accordingly. Also, removed un-necessary files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var app = module.exports = loopback();
-var appInsights = require('applicationinsights');
+const loopback = require('loopback')
+const boot = require('loopback-boot')
+const app = module.exports = loopback()
+const appInsights = require('applicationinsights')
 
 // Setting up application insights only for production
 if (app.get('env') === 'production') {
@@ -13,64 +13,63 @@ if (app.get('env') === 'production') {
     .setAutoCollectPerformance(true)
     .setAutoCollectExceptions(true)
     .setAutoCollectDependencies(true)
-    .start();
+    .start()
 }
 
 // Create an instance of PassportConfigurator with the app instance
-var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
-var passportConfigurator = new PassportConfigurator(app);
+const PassportConfigurator = require('loopback-component-passport').PassportConfigurator
+const passportConfigurator = new PassportConfigurator(app)
 // Load the provider configurations according to environment
-var config = {};
+let config = {}
 try {
   switch (app.get('env')) {
     case 'production' :
-      config = require('../providers.' + app.get('env') + '.json');
-      break;
+      config = require('../providers.' + app.get('env') + '.json')
+      break
     default:
-      config = require('../providers.json');
+      config = require('../providers.json')
   }
 } catch (err) {
-  console.trace(err);
-  process.exit(1); // fatal
+  console.trace(err)
+  process.exit(1) // fatal
 }
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+  return app.listen(function () {
+    app.emit('started')
+    const baseUrl = app.get('url').replace(/\/$/, '')
+    console.log('Web server listening at: %s', baseUrl)
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      const explorerPath = app.get('loopback-component-explorer').mountPath
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath)
     }
-  });
-};
+  })
+}
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname, function (err) {
+  if (err) throw err
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+  if (require.main === module) { app.start() }
+})
 
 // Passport needs to be configured after application has booted. Otherwise, models in app.models will not be present
 // Initialize passport
-passportConfigurator.init();
+passportConfigurator.init()
 
 // Set up related models
 passportConfigurator.setupModels({
   userModel: app.models.user,
   userIdentityModel: app.models.userIdentity,
-  userCredentialModel: app.models.userCredential,
-});
+  userCredentialModel: app.models.userCredential
+})
 
 // Configure passport strategies for third party auth providers
-for (var s in config) {
-  var c = config[s];
-  c.session = c.session !== false;
-  passportConfigurator.configureProvider(s, c);
+for (const s in config) {
+  const c = config[s]
+  c.session = c.session !== false
+  passportConfigurator.configureProvider(s, c)
 }
