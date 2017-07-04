b12e337128bf99b1904fe5a3d2a95319cd5f39c6
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-04 09:16:27 +0530

    Reverted to default index.html response due to app insights throwing error, the reason which is appollo graphql server subscription issues.

--- server/boot/root.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by rprat on 04-07-2017.
- */
-
-module.exports = function (app) { // Install a `/` route that returns app status
-  const router = app.loopback.Router()
-  router.get('/', app.loopback.status())
-  app.use(router)
-}
