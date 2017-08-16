821d8af56fce9d894707594e75307d6100eec4e5
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-16 20:40:44 +0530

    Removed dummy files and drafted the genres to show to the users from the list of spotify artist genres

---
+++ common/models/genres.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = function (Genres) {
+  /**
+   * gets the list of available genres available to viewer for filtering recommendations and results
+   * @param {Function(Error, array)} callback
+   */
+
+  Genres.getGenres = function (callback) {
+    let genres
+    // TODO
+    callback(null, genres)
+  }
+}
