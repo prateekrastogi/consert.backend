0045b1ee0ce44ffd3113125fee8f8e6e751f67ce
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-17 20:31:16 +0530

    Written get genres remote method that returns list of available genres available to user for filtering

--- common/models/genres.js
+++ common/models/genres.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const genresList = require('../../lib/genres')
+const _ = require('lodash')
+
 module.exports = function (Genres) {
   /**
    * gets the list of available genres available to viewer for filtering recommendations and results
@@ -7,8 +10,14 @@ module.exports = function (Genres) {
    */
 
   Genres.getGenres = function (callback) {
-    let genres
-    // TODO
+    let genres = ['All']
+
+    const customGenres = _.map(_.keys(genresList.genres), genres => {
+      const genre = _.replace(genres, 'And', ' & ')
+      return _.replace(genre, 'dash', '-')
+    })
+    genres = _.concat(genres, _.sortBy(customGenres))
+
     callback(null, genres)
   }
 }
