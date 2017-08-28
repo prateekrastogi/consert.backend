4edfc8c053b50cdf1001c245061232ae8f96c9d3
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-28 22:34:40 +0530

    Saving un-saved changes

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -1,5 +1,16 @@
 'use strict'
 
-module.exports = function (Recommendations) {
+module.exports = function (recommendations) {
+  /**
+   * gets the recommendation based on genreItems
+   * @param {array} genres List of genres for which to get recommendations
+   * @param {Function(Error, array)} callback
+   */
+
+  recommendations.getGenreBasedRecommendations = function (genres, callback) {
+    var recommendations
+    // TODO
+    callback(null, recommendations)
+  }
 
 }
--- common/models/search.js
+++ common/models/search.js
@@ -1,5 +1,5 @@
 'use strict'
 
-module.exports = function (Search) {
+module.exports = function (search) {
 
 }
