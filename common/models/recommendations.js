61a2366e5ee90fcf0c0a8959c3bb719d48ab1acb
Prateek Rastogi <p.rastogi@outlook.com>
2017-10-26 03:10:46 +0530

    generic method for getting recommendations

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -5,13 +5,13 @@ const recombeeRqs = require('recombee-api-client').requests
 
 module.exports = function (recommendations) {
   /**
-   * gets the recommendation based on genreItems
-   * @param {array} genres List of genres for which to get recommendations
+   * get the recommendation based on filters
+   * @param {object} filters for recommendations
    * @param {object} express http request object
    * @param {Function(Error, array)} callback
    */
 
-  recommendations.getGenreBasedRecommendations = function (genres, req, options) {
+  recommendations.getRecommendations = function (filter, req, options) {
     var recommendations
     console.log(JSON.stringify(req.headers.cookie))
     console.log(JSON.stringify(options))
