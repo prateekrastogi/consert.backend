21315d8158b53cdaebf6f440726277d4d7b441ab
Prateek Rastogi <p.rastogi@outlook.com>
2017-10-26 00:33:02 +0530

    Supported browserId

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -7,12 +7,13 @@ module.exports = function (recommendations) {
   /**
    * gets the recommendation based on genreItems
    * @param {array} genres List of genres for which to get recommendations
+   * @param {string} browserId Unique identity of user's browser
    * @param {Function(Error, array)} callback
    */
 
-  recommendations.getGenreBasedRecommendations = function (genres, options) {
+  recommendations.getGenreBasedRecommendations = function (genres, browserId, options) {
     var recommendations
-    console.log(JSON.stringify(options.accessToken))
+    console.log(JSON.stringify(JSON.stringify(options)))
     // TODO
     return new Promise((resolve, reject) => resolve(recommendations))
   }
