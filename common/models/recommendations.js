a8484809da17e4c4c62de6b678766cb5dc7d1091
Prateek Rastogi <p.rastogi@outlook.com>
2017-10-26 01:07:56 +0530

    Supported option to read cookies in remote method via express http request object as remote-method parameter. Also, used options parameter to get the current user context.

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -7,13 +7,14 @@ module.exports = function (recommendations) {
   /**
    * gets the recommendation based on genreItems
    * @param {array} genres List of genres for which to get recommendations
-   * @param {string} browserId Unique identity of user's browser
+   * @param {object} express http request object
    * @param {Function(Error, array)} callback
    */
 
-  recommendations.getGenreBasedRecommendations = function (genres, browserId, options) {
+  recommendations.getGenreBasedRecommendations = function (genres, req, options) {
     var recommendations
-    console.log(JSON.stringify(JSON.stringify(options)))
+    console.log(JSON.stringify(req.headers.cookie))
+    console.log(JSON.stringify(options))
     // TODO
     return new Promise((resolve, reject) => resolve(recommendations))
   }
