39eb07f16df870461581906ff69530b7716749c7
Prateek Rastogi <p.rastogi@outlook.com>
2017-10-27 23:06:43 +0530

    Few remote-method additions

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -8,7 +8,7 @@ module.exports = function (recommendations) {
    * get the recommendation based on filters
    * @param {object} filters for recommendations
    * @param {object} express http request object
-   * @param {Function(Error, array)} callback
+   * @param {object} options object to get accessToken, thus, always, non-fabricated authenticated user object
    */
 
   recommendations.getRecommendations = function (filter, req, options) {
@@ -17,5 +17,18 @@ module.exports = function (recommendations) {
     console.log(JSON.stringify(options))
     // TODO
     return new Promise((resolve, reject) => resolve(recommendations))
+  }
+
+  /**
+   * Registers the user's interaction with recommended item
+   * @param {string} itemId Item Id of the item interacted with
+   * @param {string} action The type of interaction action performed on that item by that user
+   * @param {object} express http request object
+   * @param {object} options object to get accessToken, thus, always, non-fabricated authenticated user object
+   */
+
+  recommendations.logUserItemInteraction = function (itemId, action, req, options) {
+    // TODO
+    return new Promise()
   }
 }
