316dd1c8b40897e7fb8a1e1c8026e72e3fc6face
Prateek Rastogi <p.rastogi@outlook.com>
2017-11-08 00:07:13 +0530

    Added functionality to get user_based and item_based recommendations and log user-item interactions :-)))

--- common/models/recommendations.js
+++ common/models/recommendations.js
@@ -6,19 +6,38 @@ const cookie = require('cookie')
 const Rx = require('rxjs')
 const _ = require('lodash')
 
+const RETRY_COUNT = 3
+
 module.exports = function (recommendations) {
   /**
    * get the recommendation based on filters
-   * @param {object} filters for recommendations
+   * @param {string} Type of recommendation i.e. USER_BASED or ITEM_BASED
+   * @param {number} Total no. of recommendations to be fetched
+   * @param {object} Parameters to customize the returned recommendations
    * @param {object} express http request object
    * @param {object} options object to get accessToken, thus, always, non-fabricated authenticated user object
    */
 
-  recommendations.getRecommendations = function (filter, req, options) {
+  recommendations.getRecommendations = function (recType, count, recParams, req, options) {
     const userId = getRecombeeUser(req, options)
-    var recommendations
-    // TODO
-    return new Promise((resolve, reject) => resolve(recommendations))
+    let recommendationObservable
+    const clientSendAsObservable = Rx.Observable.bindNodeCallback(recombeeClient.send.bind(recombeeClient))
+
+    switch (_.toUpper(recType)) {
+      case 'USER_BASED':
+        recommendationObservable = clientSendAsObservable(new recombeeRqs.UserBasedRecommendation(userId, count, recParams))
+        break
+      case 'ITEM_BASED':
+        const {itemId} = recParams
+        recommendationObservable = clientSendAsObservable(new recombeeRqs.ItemBasedRecommendation(itemId, count, _.omit(recParams, 'itemId')))
+        break
+      default:
+        throw new Error('Invalid Recommendation Type')
+    }
+
+    const recommendationPromise = recommendationObservable.retry(RETRY_COUNT).toPromise()
+
+    return new Promise((resolve, reject) => resolve(recommendationPromise))
   }
 
   /**
@@ -34,11 +53,11 @@ module.exports = function (recommendations) {
 
     const clientSendAsObservable = Rx.Observable.bindNodeCallback(recombeeClient.send.bind(recombeeClient))
 
-    const userGetAndUpdate = clientSendAsObservable(new recombeeRqs.GetUserValues(userId)).retry(3)
+    const userGetAndUpdate = clientSendAsObservable(new recombeeRqs.GetUserValues(userId)).retry(RETRY_COUNT)
       .catch(err => Rx.Observable.of(getUserUpdates(browserId, clientId, userId)))
       .concatMap((user) => {
         const mergedUser = mergerUserUpdates(user, getUserUpdates(browserId, clientId, userId))
-        return clientSendAsObservable(new recombeeRqs.SetUserValues(userId, mergedUser, {'cascadeCreate': true})).retry(3)
+        return clientSendAsObservable(new recombeeRqs.SetUserValues(userId, mergedUser, {'cascadeCreate': true})).retry(RETRY_COUNT)
       })
 
     return new Promise((resolve, reject) => resolve(userGetAndUpdate.toPromise().then(val => val === 'ok')))
@@ -48,30 +67,40 @@ module.exports = function (recommendations) {
    * Registers the user's interaction with recommended item
    * @param {string} itemId Item Id of the item interacted with
    * @param {string} action The type of interaction action performed on that item by that user
+   * @param {object} Parameters detailing the action
    * @param {object} express http request object
    * @param {object} options object to get accessToken, thus, always, non-fabricated authenticated user object
    */
 
   recommendations.logUserItemInteraction = function (itemId, action, actionParams, req, options) {
     const userId = getRecombeeUser(req, options)
-
+    let actionLoggingObservable
     const clientSendAsObservable = Rx.Observable.bindNodeCallback(recombeeClient.send.bind(recombeeClient))
 
     switch (_.toUpper(action)) {
       case 'DETAIL_VIEW':
+        actionLoggingObservable = clientSendAsObservable(new recombeeRqs.AddDetailView(userId, itemId, actionParams))
         break
       case 'PURCHASE':
+        actionLoggingObservable = clientSendAsObservable(new recombeeRqs.AddPurchase(userId, itemId, actionParams))
         break
       case 'RATING':
+        const {rating} = actionParams
+        actionLoggingObservable = clientSendAsObservable(new recombeeRqs.AddRating(userId, itemId, rating, _.omit(actionParams, 'rating')))
         break
       case 'CART_ADDITION':
+        actionLoggingObservable = clientSendAsObservable(new recombeeRqs.AddCartAddition(userId, itemId, actionParams))
         break
       case 'BOOKMARK':
+        actionLoggingObservable = clientSendAsObservable(new recombeeRqs.AddBookmark(userId, itemId, actionParams))
         break
+      default:
+        throw new Error('Invalid Action Type')
     }
 
-    // TODO
-    return new Promise()
+    const actionLoggingPromise = actionLoggingObservable.retry(RETRY_COUNT).toPromise()
+
+    return new Promise((resolve, reject) => resolve(actionLoggingPromise.then(val => val === 'ok')))
   }
 
   function mergerUserUpdates (user, userUpdates) {
