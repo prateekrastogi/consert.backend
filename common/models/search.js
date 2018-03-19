60eb083a8246b85fd4d09c8e145adefcffd029af
Prateek Rastogi <prateek.rastogi@timesinternet.in>
2018-03-19 13:10:31 +0530

    adding elasticsearch search functionality

--- common/models/search.js
+++ common/models/search.js
@@ -1,5 +1,14 @@
 'use strict'
 
-module.exports = function (search) {
+const Rx = require('rxjs')
+const elasticClient = require('../../lib/login-assist').elasticLogin()
 
+module.exports = function (search) {
+  search.search = function (params) {
+    const searchObservable = Rx.Observable.bindNodeCallback(elasticClient.search.bind(elasticClient))
+    searchObservable(params).subscribe(x => console.log(x))
+    var searchResults
+    // TODO
+    return new Promise((resolve, reject) => resolve(searchResults))
+  }
 }
