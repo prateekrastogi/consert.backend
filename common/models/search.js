ad221322568b61a930ac1b2b218a2180566012a2
Prateek Rastogi <prateek.rastogi@timesinternet.in>
2018-07-12 12:13:27 +0530

    updated to new rxjs

--- common/models/search.js
+++ common/models/search.js
@@ -1,17 +1,19 @@
 'use strict'
 
-const Rx = require('rxjs-compat')
 const R = require('ramda')
 const elasticClient = require('../../lib/login-assist').elasticLogin()
+const {map, concatMap} = require('rxjs/operators')
+const { from, bindNodeCallback } = require('rxjs')
 
 const RETRY_COUNT = 3
 
 module.exports = function (search) {
   search.search = function (params) {
-    const searchObservable = Rx.Observable.bindNodeCallback(elasticClient.search.bind(elasticClient))
+    const searchObservable = bindNodeCallback(elasticClient.search.bind(elasticClient))
     const searchResultObservable = searchObservable(params)
-      .map(R.compose(R.pluck('hits'), R.pluck('hits'), R.filter(R.has('hits'))))
-      .concatMap(results => Rx.Observable.from(results))
+      .pipe(map(R.compose(R.pluck('hits'), R.pluck('hits'), R.filter(R.has('hits')))),
+        concatMap(results => from(results))
+      )
 
     const searchResultPromise = searchResultObservable.retry(RETRY_COUNT).toPromise()
 
